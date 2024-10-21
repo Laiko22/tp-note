c7a83bd1e5bc5a53b62a9767bb4c0af96343a6bc
adril <adril@MSI>
2024-10-21 10:12:59 +0100

    Exercice 2

--- Exercice Adrien/Project1/main.c
+++ Exercice Adrien/Project1/main.c
@@ -1,9 +1,15 @@
 //Adrien Gillet
 
 #include <stdio.h>
+#define taillemax 9
 
-int taillemax = 9;
 int taille;
+int colonne = 0;
+int ligne = 0;
+int valeur = 0;
+int grille[taillemax][taillemax] = { 0 };
+int i;
+int j;
 
 void taillegrille() {
     
@@ -14,16 +20,27 @@ void taillegrille() {
         printf("La taille de la grille est trop grande (max 9)\n");
     }
     else {
+
+        printf("Quelle valeure voulez vous entrer dans votre tableau: ");
+        scanf_s("%d", &valeur);
+        printf("A quelle ligne voulez vous assigner votre valeur : ");
+        scanf_s("%d", &ligne);
+        printf("A quelle colonne voulez vous assigner votre valeur : ");
+        scanf_s("%d", &colonne);
+
+        grille[ligne-1][colonne-1] = valeur;
+
         for (int i = 0; i < taille; i++) { 
 
             printf("\n");
 
             for (int j = 0; j < taille; j++) {
-                printf("| 0 ");
+                printf("| %d ",grille[i][j]);
             }
 
             printf("|");
         }
+        
     }
 }
 
