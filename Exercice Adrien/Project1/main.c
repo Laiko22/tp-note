7d7957057ada1323f4fb73e880198b77567a1f9c
adril <adril@MSI>
2024-10-21 21:23:45 +0200

    Exercice 6

--- Exercice Adrien/Project1/main.c
+++ Exercice Adrien/Project1/main.c
@@ -1,97 +1,139 @@
-//Adrien Gillet
-
 #include <stdio.h>
-#define taillemax 9
+#include <stdlib.h>
+#include <time.h>
 
+#define taillemax 9
 
 int taille;
 int colonne = 0;
 int ligne = 0;
 int valeur = 0;
 int grille[taillemax][taillemax] = { 0 };
-int i;
-int j;
-int verif;
+int verif = 0;
+int niveau;
 
-void taillegrille() {
-    
-    printf("Entrer la taille de votre grille : ");
-    scanf_s("%d", &taille);
+void aleatoire() {
+    int totalCases = taille * taille;
+    int casesremplir;
 
-    while (verif != 1) {
+    printf("Entrer le niveau de difficulte voulu (1-3) : ");
+    scanf_s("%d", &niveau);
 
-        if (taille > taillemax)
-            printf("La taille de la grille est trop grande (max 9)\n");
+    if (niveau < 1 || niveau > 3) {
+        printf("Niveau invalide.\n");
+        return;
+    }
 
-        else {
+    switch (niveau) {
+    case 1:
+        casesremplir = totalCases / 2;
+        break;
+    case 2:
+        casesremplir = totalCases / 3;
+        break;
+    case 3:
+        casesremplir = totalCases / 4;
+        break;
+    }
 
-            printf("Quelle valeure voulez vous entrer dans votre tableau: ");
-            scanf_s("%d", &valeur);
-            printf("A quelle ligne voulez vous assigner votre valeur : ");
-            scanf_s("%d", &ligne);
-            printf("A quelle colonne voulez vous assigner votre valeur : ");
-            scanf_s("%d", &colonne);
+    srand((unsigned int)time(NULL));
+    int remplissage = 0;
 
-            if (ligne >= 0 && ligne < taille && colonne >= 0 && colonne < taille) {
+    while (remplissage < casesremplir) {
+        int ligne = rand() % taille;
+        int colonne = rand() % taille;
+        int valeur = (rand() % taille) + 1;
 
-                grille[ligne - 1][colonne - 1] = valeur;
-                verif = 1;
+        if (grille[ligne][colonne] == 0) {
+            int deja = 0;
 
+            for (int i = 0; i < taille; i++) {
+                if (grille[ligne][i] == valeur || grille[i][colonne] == valeur) {
+                    deja = 1;
+                    break;
+                }
             }
 
-            else
-            
-                printf("\n############################################\nValeurs incoherentes, veuillez recommencer :\n############################################\n\n");
-
+            if (!deja) {
+                grille[ligne][colonne] = valeur;
+                remplissage++;
+            }
         }
     }
 }
+
+void taillegrille() {
+    printf("Entrer la taille de votre grille (max %d) : ", taillemax);
+    scanf_s("%d", &taille);
+
+    if (taille < 1 || taille > taillemax) {
+        printf("La taille de la grille est invalide (entre 1 et %d).\n", taillemax);
+        return;
+    }
+
+    printf("Quelle valeur voulez-vous entrer dans votre tableau : ");
+    scanf_s("%d", &valeur);
+    printf("A quelle ligne voulez-vous assigner votre valeur (1 a %d) : ", taille);
+    scanf_s("%d", &ligne);
+    printf("A quelle colonne voulez-vous assigner votre valeur (1 a %d) : ", taille);
+    scanf_s("%d", &colonne);
+
+    if (ligne >= 1 && ligne <= taille && colonne >= 1 && colonne <= taille) {
+        grille[ligne - 1][colonne - 1] = valeur;
+    }
+    else {
+        printf("Valeurs incohérentes, veuillez recommencer.\n");
+    }
+}
+
 void verification() {
-
-    if (colonne && ligne > taille)
-        printf("Erreur de placement de la valeur");
-
-    if (taille > taillemax)
-        printf("Erreur de taille du cadre");
-
     for (int i = 0; i < taille; i++) {
-
         int valeursligne[taillemax] = { 0 };
 
         for (int j = 0; j < taille; j++) {
-
-            if (valeursligne[grille[i][j] - 1] == 1)
-
-                printf("Erreur : La valeur %d est repetee ");
-
+            if (grille[i][j] > 0) {
+                if (valeursligne[grille[i][j] - 1] == 1) {
+                    printf("Erreur : La valeur %d est repetee a la ligne %d\n", grille[i][j], i + 1);
+                }
+                else {
+                    valeursligne[grille[i][j] - 1] = 1;
+                }
+            }
         }
     }
 }
+
 void affichage() {
 
+    for (int i = 0; i < taille; i++) 
+
+        printf("----");
+
+    printf("-\n");
+
     for (int i = 0; i < taille; i++) {
-
-        printf("\n");
-
         for (int j = 0; j < taille; j++) {
-
             if (grille[i][j] == 0)
                 printf("|   ");
-                
-            else 
+            else
                 printf("| %d ", grille[i][j]);
         }
+        printf("|\n");
 
-        printf("|");
+        for (int j = 0; j < taille; j++) 
+
+            printf("----");
+        
+        printf("-\n");
     }
 }
-
 
 int main() {
 
     taillegrille();
+    aleatoire();
+    verification();
     affichage();
 
     return 0;
 }
-
