e638a71b47011beabc232ddcdd196a9b4a42cf92
adril <adril@MSI>
2024-10-21 09:09:22 +0100

    exotest

---
+++ Exercice Adrien/Project1/main.c
@@ -0,0 +1,32 @@
+//Adrien Gillet
+
+#include <stdio.h>
+
+int taillemax = 9;
+int taille;
+
+void taillegrille() {
+    
+    printf("Entrer la taille de votre grille : ");
+    scanf_s("%d", &taille);
+
+    if (taille > taillemax) {
+        printf("La taille de la grille est trop grande (max 9)\n");
+    }
+    else {
+        for (int i = 0; i < taille; i++) {
+            printf("\n");
+            for (int j = 0; j < taille; j++) {
+                printf("| ");
+            }
+        }
+    }
+}
+
+int main() {
+
+    taillegrille();
+
+    return 0;
+}
+
